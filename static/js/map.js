2753a8585d05008d976426f0bd1f23046076963a
Lyzi Diamond <lyzidiamond@gmail.com>
2014-09-18 11:49:38 -0700

    Add static and templates

---
+++ static/js/map.js
@@ -0,0 +1,23 @@
+// Create map object
+window.MapBuilder = {};
+
+// initialize map
+MapBuilder.init = function(id) {
+  // Create map container
+  var leafletMap = L.mapbox.map(id, 'codeforamerica.jfnj92nm', {maxZoom: 17, minZoom: 10, accessToken: 'pk.eyJ1IjoiY29kZWZvcmFtZXJpY2EiLCJhIjoiSTZlTTZTcyJ9.3aSlHLNzvsTwK-CYfZsG_Q'}).setView([38.03,-84.509], 12);
+
+  // Make layer group
+  var addresses = L.layerGroup();
+
+  return {
+    addToMap: function (data) {
+      addresses.clearLayers();
+      var geoJsonResults = L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup(feature.properties.formatted_address);
+        }
+      });
+      addresses.addLayer(geoJsonResults).addTo(leafletMap);
+    }
+  }
+}
