24dcc9ec13b0a219f8603e8b0992929e213f9873
Jeremia Kimelman <jbkimelman@gmail.com>
2014-10-14 13:03:01 -0700

    batch endpoints making csv in mem

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from collections import OrderedDict
+import csv
 import json
 import os
-from flask import Flask, render_template, jsonify, request
+
+from flask import Flask, render_template, jsonify, request, Response
 from elasticsearch import Elasticsearch
 from urlparse import urlparse
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -60,5 +64,42 @@ def geocode():
   query = request.args['query']
   return jsonify(likely_parcels(query))
 
+@app.route('/geocode_batch', methods=['POST'])
+def geocode_batch():
+  input_file = request.files.get('query', False)
+  geocoded = {}
+  if input_file is not False:
+    csv_reader = csv.DictReader(input_file)
+    temp_csv = open('tmp.csv', 'w')
+    csv_writer = csv.writer(temp_csv)
+    next(csv_reader, None) # skip headers
+    outsideRows = OrderedDict
+
+    outsideRows.append(csv_reader[0])
+
+    for index, row in enumerate(csv_reader):
+        ordered_row = ['%s' % (row[fieldname]) for fieldname in csv_reader.fieldnames]
+        print 'geocoding number %s' % index
+        address = row['ADDRESS']
+
+        print 'address: %s' % address
+
+        results = search_for(address)
+        if results['total'] != 0:
+          result = results['hits'][0]
+
+          ordered_row.append(result['_source']['X'])
+          ordered_row.append(result['_source']['Y'])
+
+        outsideRows.append(ordered_row)
+
+    def generate():
+      for row in outsideRows:
+        yield ','.join(row) + '\n'
+
+    return Response(generate(), mimetype='text/csv')
+  else:
+    return 'attach file', 400
+
 if __name__ == ('__main__'):
-  app.run()
+  app.run(debug=True)
